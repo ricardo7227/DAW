c61c50411c95dc0b1d64b822a9f04637cfa64cd0
ricardo7227 <r.alexis.remache@gmail.com>
2017-12-04 12:41:18 +0100

    Actualización JAVA

---
+++ Servidor/databaseJavaPool/src/main/java/utils/UrlsPaths.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author daw
+ */
+public class UrlsPaths {
+
+    public static String alumnos = "alumnos";
+    public static String asignaturas = "asignaturas";
+    public static String notas = "notas";
+    public static String registro = "registro";
+}
