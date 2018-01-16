e6c2f7866194d0485a4ad39794cca7ebf509fbd7
ricardo7227 <r.alexis.remache@gmail.com>
2018-01-16 12:03:05 +0100

    Consumir API, inicio del proyecto

---
+++ Servidor/Consumir Api/ConsumirApiEMTJava/src/main/java/utilidades/Api.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+/**
+ *
+ * @author daw
+ */
+public class Api {
+    public static final String BASE_URL_EMT = "https://openbus.emtmadrid.es:9443/emt-proxy-server/last";
+    
+    //end-points
+    public static String END_POINT_GET_ARRIVE_STOP = BASE_URL_EMT + "/geo/GetArriveStop.php";
+    
+    
+    
+}
---
+++ Servidor/Consumir Api/ConsumirApiEMTJava/src/main/java/utilidades/Constantes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+/**
+ *
+ * @author daw
+ */
+public class Constantes {
+    public static String  LINE = "line";
+    public static String  LINES = "Lines";
+    public static String  SELECT_DATE = "SelectDate";
+    public static String  ID_STOP = "idStop";
+    public static String  FECHA = "fecha";
+    
+    
+}
